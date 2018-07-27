f46e50b01c3e90698937632c6dc33b7587b27b91
grondag <grondag@gmail.com>
2018-07-27 08:39:55 -0700

    Clean up config

--- src/main/java/grondag/render_hooks/Configurator.java
+++ src/main/java/grondag/render_hooks/Configurator.java
@@ -1,6 +1,5 @@
 package grondag.render_hooks;
 
-import grondag.render_hooks.core.LightingModel;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Config.Comment;
 import net.minecraftforge.common.config.Config.LangKey;
@@ -17,19 +16,15 @@ public class Configurator
 {
     @RequiresMcRestart
     @Comment({"Maximum number of render pipelines that can be registered at runtime.",
-        " Smaller values will save slightly on memory overhead.  It isn't very much but",
+        " The value is fixed at startup to enable very fast lookups.",
+        " Smaller values will save slightly on memory overhead.  It isn't much but",
         " is configurable for those folks who like to save every byte possible...."})
     @RangeInt(min = 16, max = 1024)
     public static int maxPipelines = 64;
     
     @Comment({"Changing will reload all renderers and models.",
         " Has no effect if mod is disabled because of ASM failures. ",
-        " Primary use is for testing."})
+        " Primary use is for testing. Note that VBOs will be enabled",
+        " if mod is enabked, regardless of Minecraft configuration."})
     public static boolean enabled = true;
-    
-    @Comment({"Lighting model used for rendering.",
-         " Changing will reload all renderers and models.",
-         " Has no effect if mod is disabled because of ASM failures."})
-    public static LightingModel lightingModel = LightingModel.CLASSIC;
-
 }
