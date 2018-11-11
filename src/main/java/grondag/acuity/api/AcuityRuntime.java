48df411fb507c697992a0fee6a6ad02fb8ebb8d1
grondag <grondag@gmail.com>
2018-11-10 16:09:28 -0800

    Revert "Buffer allocation utilities"
    This reverts commit c0e8acb10273ae2c6ea9dd992b5fab91f1db60d9.

--- src/main/java/grondag/acuity/api/AcuityRuntime.java
+++ src/main/java/grondag/acuity/api/AcuityRuntime.java
@@ -6,8 +6,6 @@ import java.util.Iterator;
 import java.util.function.Consumer;
 
 import grondag.acuity.Acuity;
-import grondag.acuity.buffering.BufferAllocator;
-import grondag.acuity.buffering.BufferSlice;
 import grondag.acuity.buffering.MappedBufferStore;
 import grondag.acuity.core.PipelineShaderManager;
 import grondag.acuity.hooks.PipelineHooks;
@@ -44,8 +42,6 @@ public final class AcuityRuntime implements IAcuityRuntime
         PipelineShaderManager.INSTANCE.forceReload();
         PipelineManager.INSTANCE.forceReload();
         PipelineHooks.forceReload();
-        BufferSlice.initialize();
-        BufferAllocator.initialize();
         MappedBufferStore.forceReload();
         AcuityRuntime.INSTANCE.forEachListener(c -> c.onRenderReload());
     }
