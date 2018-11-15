d1c5bda223927bec461272f1c1b42d54935c517f
grondag <grondag@gmail.com>
2018-11-15 06:54:52 -0800

    More garbage reduction in chunk rebuild

--- src/main/java/grondag/acuity/mixin/MixinBlock.java
+++ src/main/java/grondag/acuity/mixin/MixinBlock.java
@@ -4,18 +4,27 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import grondag.acuity.hooks.BlockHooks;
 import net.minecraft.block.Block;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
 
 @Mixin(Block.class)
 public abstract class MixinBlock
 {
+    private static ThreadLocal<MutableBlockPos> offsetPos = new ThreadLocal<MutableBlockPos>()
+    {
+        @Override
+        protected MutableBlockPos initialValue()
+        {
+            return new MutableBlockPos();
+        }
+    };
+    
     // prevents significant garbage build up during chunk rebuild
     @Redirect(method = "shouldSideBeRendered", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;offset(Lnet/minecraft/util/EnumFacing;)Lnet/minecraft/util/math/BlockPos;"))
     private BlockPos onOffset(BlockPos pos, EnumFacing facing)
     {
-        return BlockHooks.onOffset(pos, facing);
+        return offsetPos.get().setPos(pos).move(facing);
     }
 }
