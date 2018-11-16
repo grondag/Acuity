aa7f0e68276dc086cc9ad76b043592fcbbd1de5a
grondag <grondag@gmail.com>
2018-11-16 07:54:12 -0800

    Garbage reduction

---
+++ src/main/java/grondag/acuity/mixin/MixinForgeHooksClient.java
@@ -0,0 +1,30 @@
+package grondag.acuity.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
+import net.minecraftforge.client.ForgeHooksClient;
+
+@Mixin(ForgeHooksClient.class)
+public class MixinForgeHooksClient
+{
+    private static ThreadLocal<MutableBlockPos> posAdd = new ThreadLocal<MutableBlockPos>()
+    {
+        @Override
+        protected MutableBlockPos initialValue()
+        {
+            return new MutableBlockPos();
+        }
+    };
+    
+    // prevents significant garbage build up during chunk load
+    @Redirect(method = "getSkyBlendColour", expect = 1, at = @At(value = "INVOKE", 
+            target = "Lnet/minecraft/util/math/BlockPos;add(III)Lnet/minecraft/util/math/BlockPos;"))
+    private static BlockPos onGetSkyBlendColourPosAdd(BlockPos pos, int x, int y, int z)
+    {
+        return posAdd.get().setPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
+    }
+}
